db362e2607d3bf788b74bbb204cbb323a3563850
sergii <ssolsona9@gmail.com>
2026-02-11 05:27:15 +0100

    Add CreateSong route

---
+++ src/views/CreateSong.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold">Crear tu canción</h1>
+    <p class="opacity-70 mt-2">Pantalla en construcción 🙂</p>
+  </div>
+</template>
+
+<script>
+export default { name: 'CreateSong' }
+</script>
