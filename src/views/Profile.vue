c49888741778739702b33978bb4b026810ac3712
sergii <ssolsona9@gmail.com>
2026-02-14 22:57:53 +0100

    fix(profile): use ?url query for asset import

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -7,7 +7,7 @@ import { useFollows } from '../stores/follows'
 import ThemeToggle from '../components/ThemeToggle.vue'
 import { useThemeStore } from '../stores/theme'
 import ChatModal from '../components/ChatModal.vue'
-import loadingImg from '../assets/circu-Photoroom.png'
+import loadingImg from '../assets/circu-Photoroom.png?url'
 
 const loadingImgUrl = loadingImg
 
